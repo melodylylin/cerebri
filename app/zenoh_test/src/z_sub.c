9d7bf0fdb7952b92591d07fd0553f1f51e4db126
James Goppert <james.goppert@gmail.com>
2023-11-12 14:45:40 -0500

    Work on zenoh.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- app/zenoh_test/src/z_sub.c
+++ app/zenoh_test/src/z_sub.c
@@ -14,33 +14,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <zenoh-pico.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/shell/shell.h>
 
-#if Z_FEATURE_SUBSCRIPTION == 1
-#define CLIENT_OR_PEER 0 // 0: Client mode; 1: Peer mode
-#if CLIENT_OR_PEER == 0
-#define MODE "client"
-#define CONNECT "" // If empty, it will scout
-#elif CLIENT_OR_PEER == 1
+extern struct k_work_q g_low_priority_work_q;
+
 #define MODE "peer"
-#define CONNECT "udp/224.0.0.225:7447#iface=en0"
-#else
-#error "Unknown Zenoh operation mode. Check CLIENT_OR_PEER value."
-#endif
+//#define CONNECT "udp/192.0.2.1:7447@iface=eth0"
+//#define CONNECT "udp/224.0.0.225:7447#iface=en0"
+#define CONNECT "udp/224.0.0.225:7447#iface=eth0"
+
+//#define MODE "client"
+//#define CONNECT ""
 
 #define KEYEXPR "demo/example/**"
 
-void data_handler(const z_sample_t* sample, void* arg)
-{
+LOG_MODULE_REGISTER(main, CONFIG_CEREBRI_LOG_LEVEL);
+
+static void data_handler(const z_sample_t *sample, void *arg) {
     z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(" >> [Subscriber handler] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
-        sample->payload.start);
+    LOG_WRN(" >> [Subscriber handler] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+           sample->payload.start);
     z_drop(z_move(keystr));
 }
 
-int main(int argc, char** argv)
-{
-    sleep(5);
-
+void zenoh_work_handler(struct k_work* work) {
     // Initialize Zenoh Session and other parameters
     z_owned_config_t config = z_config_default();
     zp_config_insert(z_loan(config), Z_CONFIG_MODE_KEY, z_string_make(MODE));
@@ -49,32 +47,32 @@ int main(int argc, char** argv)
     }
 
     // Open Zenoh session
-    printf("Opening Zenoh Session...");
+    LOG_WRN("Opening Zenoh Session...");
     z_owned_session_t s = z_open(z_move(config));
     if (!z_check(s)) {
-        printf("Unable to open session!\n");
-        exit(-1);
+        LOG_WRN("Unable to open session!\n");
+	return;
     }
-    printf("OK\n");
+    LOG_WRN("OK\n");
 
     // Start the receive and the session lease loop for zenoh-pico
     zp_start_read_task(z_loan(s), NULL);
     zp_start_lease_task(z_loan(s), NULL);
 
-    printf("Declaring Subscriber on '%s'...", KEYEXPR);
+    LOG_WRN("Declaring Subscriber on '%s'...", KEYEXPR);
     z_owned_closure_sample_t callback = z_closure(data_handler);
     z_owned_subscriber_t sub = z_declare_subscriber(z_loan(s), z_keyexpr(KEYEXPR), z_move(callback), NULL);
     if (!z_check(sub)) {
-        printf("Unable to declare subscriber.\n");
-        exit(-1);
+        LOG_WRN("Unable to declare subscriber.\n");
+	return;
     }
-    printf("OK!\n");
+    LOG_WRN("OK!\n");
 
     while (1) {
-        sleep(5);
+        k_msleep(5000);
     }
 
-    printf("Closing Zenoh Session...");
+    LOG_WRN("Closing Zenoh Session...");
     z_undeclare_subscriber(z_move(sub));
 
     // Stop the receive and the session lease loop for zenoh-pico
@@ -82,14 +80,16 @@ int main(int argc, char** argv)
     zp_stop_lease_task(z_loan(s));
 
     z_close(z_move(s));
-    printf("OK!\n");
+    LOG_WRN("OK!\n");
 
+    return;
+}
+
+K_WORK_DEFINE(zenoh_work, zenoh_work_handler);
+
+int zenoh_cmd() {
+    k_work_submit_to_queue(&g_low_priority_work_q, &zenoh_work);
     return 0;
 }
-#else
-int main(void)
-{
-    printf("ERROR: Zenoh pico was compiled without Z_FEATURE_SUBSCRIPTION but this example requires it.\n");
-    return -2;
-}
-#endif
+
+SHELL_CMD_REGISTER(zenoh, NULL, "connect zenoh", zenoh_cmd);
